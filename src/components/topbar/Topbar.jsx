01df548603c6102aa2d7e5e60be8260aa40d6516
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-12-03 15:08:08 -0600

    add cool navbar animation on click

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,9 @@
 import "./Topbar.scss"
 import {Person, Mail} from '@material-ui/icons'
 
-function Topbar() {
+function Topbar({menuOpen, setMenuOpen}) {
     return (
-        <div className='topbar'>
+        <div className={'topbar ' + (menuOpen && ' active')}>
             <div className="wrapper">
                 <div className="left">
                     <a href="#intro" className="logo">logo.</a>
@@ -17,7 +17,7 @@ function Topbar() {
                     </div>
                 </div>
                 <div className="right">
-                    <div className="hamburger">
+                    <div className="hamburger" onClick={()=> setMenuOpen(!menuOpen)}>
                         <span className="line1">1</span>
                         <span className="line2">2</span>
                         <span className="line3">3</span>
