67d077df7a7b787ab26e0a35f7d2d6c97ee3e9f8
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-12-03 15:24:16 -0600

    display sidebar on screen

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Intro from './components/intro/Intro';
 import Portfolio from './components/portfolio/Portfolio';
 import Topbar from './components/topbar/Topbar';
 import Works from './components/works/Works';
+import Menu from './menu/Menu';
 import "./app.scss"
 import react, {useState} from 'react'
 
@@ -12,6 +13,7 @@ function App() {
   return (
     <div className="app">
       <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <Menu />
       <div className="sections">
           <Intro/>
           <Portfolio/>
