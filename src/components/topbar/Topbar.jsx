779ca2c685573238a95969bcb597642bbe88486f
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-12-03 14:35:19 -0600

    style navbar components

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,11 +1,25 @@
 import "./Topbar.scss"
+import {Person, Mail} from '@material-ui/icons'
 
 function Topbar() {
     return (
-        <h1 className='topbar'>Top Bar</h1>
-        
-           
-    
+        <div className='topbar'>
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">logo.</a>
+                    <div className="itemContainer">
+                        <Person className="icon"/>
+                        <span>+913-944-6189</span>
+                    </div>
+                    <div className="itemContainer">
+                        <Mail className="icon"/> 
+                        <span>Jeff.Woltjen@gmail.com</span>
+                    </div>
+                </div>
+                <div className="right">
+                </div>
+            </div>
+        </div>
     )
 }
 
