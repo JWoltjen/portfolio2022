b9dcd322644610b9650051068f6c7286b99ea99e
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-12-04 13:27:20 -0600

    create list of portfolio projects and style them

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,20 @@ import "./Portfolio.scss"
 function Portfolio() {
     return (
         <div className="portfolio" id="portfolio">
-            Portfolio
+            <h1>Portfolio</h1>
+            <ul>
+                <li className="active">Featured</li>
+                <li>Night at the Movies</li>
+                <li>Neumann</li>
+                <li>Quiz App</li>
+                <li>Image Finder</li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="./assets/portfolio.png" alt=""/>
+                    <h2>What</h2>
+                </div>
+            </div>
         </div>
     )
 }
