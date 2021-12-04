53b361c17e5ea53bc9b91e3dd54f4173bda71450
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-12-04 12:16:24 -0600

    style right section of intro section

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -8,7 +8,16 @@ function Intro() {
                    <img src="assets/cat.png" alt="a photo of jeff"/>
                </div>
            </div>
-           <div className="right"></div>
+           <div className="right">
+                <div className="wrapper">
+                    <h2>Hi there, I'm</h2>
+                    <h1>Jeff Woltjen</h1>
+                    <h3>Freelance<span></span></h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/down.png" alt="a downward directional arrow"/>
+                </a>
+           </div>
         </div>
     )
 }
