8e977f22c8c30457a455156c312d9051993ae726
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-12-06 09:38:35 -0600

    create slider container

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -3,7 +3,18 @@ import "./Works.scss"
 function Works() {
     return (
         <div className="works" id="works">
-            Works
+            <div className="slider">
+                <div className="container">
+                    <div className="item">
+                        <div className="left">
+
+                        </div>
+                        <div className="right">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
