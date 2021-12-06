909f2e12467d3b8f9b6c9977d040a95485cea973
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-12-06 12:11:24 -0600

    create container for testimonial section with dummy card"

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -3,7 +3,23 @@ import "./Testimonials.scss"
 function Testimonials() {
     return (
         <div className='testimonials' id='testimonials'>
-            Testimonials
+            <h1>Testimonials</h1>
+            <div className="container">
+                <div className="card">
+                    <div className="top">
+                        <img src="assets/right-arrow.png" className="left" alt=""/>
+                        <img src="https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" className="" alt=""/>
+                        <img src="assets/youtube.png" alt=""/>
+                    </div>
+                    <div className="center">
+                        Deserunt voluptate adipisicing fugiat incididunt mollit cillum reprehenderit in anim magna. 
+                    </div>
+                    <div className="bottom">
+                        <h3>Jason</h3>
+                        <h4>Brother</h4>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
