3370ddc202842b27660d8b77f2d065b94a7883e5
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-12-06 12:19:55 -0600

    style testimonial dummy card with margins and fdirection column

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -8,8 +8,8 @@ function Testimonials() {
                 <div className="card">
                     <div className="top">
                         <img src="assets/right-arrow.png" className="left" alt=""/>
-                        <img src="https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" className="" alt=""/>
-                        <img src="assets/youtube.png" alt=""/>
+                        <img src="https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" className="user" alt=""/>
+                        <img src="assets/youtube.png" className="right" alt=""/>
                     </div>
                     <div className="center">
                         Deserunt voluptate adipisicing fugiat incididunt mollit cillum reprehenderit in anim magna. 
