abfff2631eda636694a1c27f085a9909bc0d1f08
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-12-06 13:15:32 -0600

    create left and right contact sections

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,17 @@ import './Contact.scss'
 function Contact() {
     return (
         <div className="contact" id="contact">
-            Contact
+            <div className="left">
+                <img src="assets/shake.svg" alt=""/>
+            </div>
+            <div className="right">
+                <h2>Contact</h2>
+                <form>
+                    <input type="text" placeholder="email"/>
+                    <textarea placeholder="Message"></textarea>
+                    <button type="submit">Send</button>
+                </form>
+            </div>
         </div>
     )
 }
