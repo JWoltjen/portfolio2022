9173cd4754e29ae4f672b397dba4f2e656ac7aa9
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-12-06 13:19:45 -0600

    add prevent default logic to submit button

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,10 @@
 import './Contact.scss'
 
 function Contact() {
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); 
+    }
     return (
         <div className="contact" id="contact">
             <div className="left">
@@ -8,7 +12,7 @@ function Contact() {
             </div>
             <div className="right">
                 <h2>Contact</h2>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <input type="text" placeholder="email"/>
                     <textarea placeholder="Message"></textarea>
                     <button type="submit">Send</button>
