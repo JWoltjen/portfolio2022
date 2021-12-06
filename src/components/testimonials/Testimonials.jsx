9c15c00a7136214349e6c60497e42661c3b37f86
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-12-06 12:29:15 -0600

    map over dummy data in testimonial section

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -38,20 +38,35 @@ function Testimonials() {
         <div className='testimonials' id='testimonials'>
             <h1>Testimonials</h1>
             <div className="container">
-                <div className="card">
-                    <div className="top">
-                        <img src="assets/right-arrow.png" className="left" alt=""/>
-                        <img src="https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" className="user" alt=""/>
-                        <img src="assets/youtube.png" className="right" alt=""/>
-                    </div>
-                    <div className="center">
-                        Deserunt voluptate adipisicing fugiat incididunt mollit cillum reprehenderit in anim magna. 
-                    </div>
-                    <div className="bottom">
-                        <h3>Jason</h3>
-                        <h4>Brother</h4>
-                    </div>
-                </div>
+                {data.map(item => (
+                   <div className="card">
+                   <div className="top">
+                       <img 
+                           src="assets/right-arrow.png" 
+                           className="left" 
+                           alt=""
+                       />
+                       <img 
+                           src={item.img}
+                           className="user" 
+                           alt=""
+                       />
+                       <img 
+                           src={item.icon} 
+                           className="right" 
+                           alt=""
+                       />
+                   </div>
+                   <div className="center">
+                       {item.desc}
+                   </div>
+                   <div className="bottom">
+                       <h3>{item.name}</h3>
+                       <h4>{item.title}</h4>
+                   </div>
+               </div>  
+                ))}
+               
             </div>
         </div>
     )
