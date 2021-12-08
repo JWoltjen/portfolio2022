f8a17042340578d9bddb5a2606adfd3c2c648013
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-12-08 11:00:29 -0600

    style portfolio section and attached data

--- src/data.js
+++ src/data.js
@@ -3,8 +3,8 @@ export const nightAtTheMovies =
         id: "nightAtTheMovies", 
         title: "Night at the Movies", 
         img: "tbd", 
-        desc: "tbd", 
-        tech: ["tbd", "tbd", 'tbd']
+        desc: "A SPA which allows users to search for movies and shows and click on the results for more details.", 
+        tech: ["Redux Toolkit", "React Router", 'React', 'Fetch']
     }]
 
 export const neumann =
@@ -12,17 +12,8 @@ export const neumann =
         id: "neumann", 
         title: "Neumann", 
         img: "tbd", 
-        desc: "tbd", 
-        tech: ["tbd", "tbd", 'tbd']
-    }]
-
-export const quizApp =
-    [{
-        id: "quizApp", 
-        title: "Quiz App", 
-        img: "tbd", 
-        desc: "tbd", 
-        tech: ["tbd", "tbd", 'tbd']
+        desc: "A MERN application powered by GPT-3 ai that allows users to click on categories of knowledge and have a small aphorism played on a card. Users can ask the AI to evaluate the quoted snippet in its own words.", 
+        tech: ["GPT-3", "Next.js", 'MongoDB', 'React']
     }]
 
 export const catter = 
@@ -30,6 +21,6 @@ export const catter =
         id: "catter", 
         title: "Catter",
         img: "tbd", 
-        desc: "tbd", 
-        tech: ["tbd", "tbd", 'tbd']
+        desc: "A SPA that allows users to search for a type of cat, fetches 10 of those cats, and allows users to leave upvotes on each fetched cat.", 
+        tech: ["React", "ReduxToolkit", 'React-Redux']
     }]
